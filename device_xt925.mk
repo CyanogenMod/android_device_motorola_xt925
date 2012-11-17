eaf51adf27158bd7ff18907f16c14c87c3017b98
Emerson Pinter <dev@pinter.com.br>
2012-11-17 20:17:11 -0200

    Fix signal bar

--- device_xt925.mk
+++ device_xt925.mk
@@ -2,7 +2,9 @@ $(call inherit-product, device/motorola/vanquish-common/device.mk)
 
 LOCAL_PATH := device/motorola/xt925
 
+#telephony
 PRODUCT_PROPERTY_OVERRIDES += \
-	telephony.lteOnGsmDevice=1
+	telephony.lteOnGsmDevice=1 \
+	ro.telephony.ril.v3=signalstrength
 
 $(call inherit-product-if-exists, vendor/motorola/xt925/xt925-vendor.mk)
