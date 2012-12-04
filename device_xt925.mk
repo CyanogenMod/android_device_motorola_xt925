99b9f7de111722b6255f2dc5a38584fb3dd35b5d
Emerson Pinter <dev@pinter.com.br>
2012-12-04 19:12:02 -0200

    Use from device gps.conf
    Change-Id: I99beff52e4dedec6776b167f39a6db13e7a072ed

--- device_xt925.mk
+++ device_xt925.mk
@@ -7,4 +7,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	telephony.lteOnGsmDevice=1 \
 	ro.telephony.ril.v3=signalstrength
 
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/config/gps.conf:system/etc/gps.conf \
+
 $(call inherit-product-if-exists, vendor/motorola/xt925/xt925-vendor.mk)
